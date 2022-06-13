061ca4618edbf2259ed79688fc6efba620815642
Vue Lee <vuelee09@gmail.com>
2022-06-13 18:42:49 -0500

    create login route

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const bcrypt = require('bcrypt');
 
 // create our User model
 // User inherits all of the functionality the Model class has
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
