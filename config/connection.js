a129bf293498cb45da9db24fac9be42bff895dde
Vue Lee <vuelee09@gmail.com>
2022-06-14 13:34:22 -0500

    add heroku JawsDB

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    lize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } 
 else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
