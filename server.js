1dec7304a88afd38b4d0e737c7b535f3ad6e4b9f
Vue Lee <vuelee09@gmail.com>
2022-06-15 13:33:32 -0500

    create login and logout functionality

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // setups handlebars as our template engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -20,6 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 // this middleware takes all contents in the folder and serve them as static assets
 // useful for front-end specific files like images and style sheets
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
