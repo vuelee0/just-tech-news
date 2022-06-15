0f4d827a0b8c850ab96badb9d2c2113de24eb723
Vue Lee <vuelee09@gmail.com>
2022-06-15 11:22:15 -0500

    create homepahge template, sequelize the data

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// setups handlebars as our template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// this middleware takes all contents in the folder and serve them as static assets
+// useful for front-end specific files like images and style sheets
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
