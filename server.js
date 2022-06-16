86fd948f9585da447df42cb5672785e81a4191c2
Vue Lee <vuelee09@gmail.com>
2022-06-15 23:19:41 -0500

    create partials, add helper functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 // setups handlebars as our template engine
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
